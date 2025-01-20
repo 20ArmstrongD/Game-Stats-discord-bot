0b378c8f1463a1f52d60ae57b093362604f0d34f
20ArmstrongD <donald.curtis.armstrong@gmail.com>
2025-01-20 19:03:33 +0000

    reworked main.py slash command and event/scrapper.py and event/keyhole.py

--- events/scrapper.py
+++ events/scrapper.py
@@ -1,5 +1,4 @@
 import logging
-# import time
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
@@ -8,8 +7,8 @@ from selenium.webdriver.firefox.options import Options
 # Logging configuration
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-
 def get_playerdata(api_url):
+
     # Configure Firefox option for headless browsing (non-ui)
     options = Options()
     options.headless = True  
@@ -17,8 +16,8 @@ def get_playerdata(api_url):
     options.set_preference("gfx.webrender.enabled", False)
     options.set_preference("layers.acceleration.disabled", True)
 
-    driver = None  # Initialize driver outside the try block
-    elements = {}  # Initialize elements dictionary
+    # Initialize driver outside the try block
+    driver = None  
 
     try:
         # Init the Firefox WebDriver using pointing to path for geckodriver
@@ -31,7 +30,7 @@ def get_playerdata(api_url):
         # Wait for the page to load, in case of dynamic content
         driver.implicitly_wait(10)
 
-        # Find the elements containing the player data
+        # Find the elements containing the player data (assuming everyplayer has these)
         try:
             logging.info(f"Looking For Player Data...")
 
@@ -42,23 +41,29 @@ def get_playerdata(api_url):
             kd = kd_element.text if kd_element else None
             level = level_element.text if level_element else None
             playtime = playtime_element.text if playtime_element else None
+
+            # try to find Ranked elements (not every player has these)
             try:
                 rank_element = driver.find_element(By.XPATH, "//span[contains(@class, 'text-20') and contains(@class, 'text-secondary')]")
                 ranked_kd_element = driver.find_element(By.XPATH, "//section[1]//div[3]//div[1]//div[2]//div[5]//span[2]/span")
+                ranked_img_element = driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div[3]/div/main/div[3]/div[2]/div[2]/div[2]/section[2]/div[2]/div[1]/div[2]/div[1]/img")
 
                 rank = rank_element.text if rank_element else None
                 ranked_kd = ranked_kd_element.text if ranked_kd_element else None
+                ranked_img = ranked_img_element.get_attribute("src") if ranked_img_element else None
             except Exception as e:
                 logging.warning("Ranked elements not found.")
                 rank = None
                 ranked_kd = None
+                ranked_img = None
 
             elements = {
                 'KD': kd,
                 'Level': level,
                 'Playtime': playtime,
                 'Rank': rank,
-                'Ranked KD': ranked_kd}
+                'Ranked KD': ranked_kd,
+                'Ranked Image': ranked_img}
 
             logging.info("Player Data Successfully Found!")
             for key, value in elements.items():
@@ -79,11 +84,9 @@ def get_playerdata(api_url):
     # non_none_elements = {key: value for key, value in elements.items() if value is not None}
     # none_elements = {key: value for key, value in elements.items() if value is None}
 
-    return kd, level, playtime, rank, ranked_kd
+    return kd, level, playtime, rank, ranked_kd, ranked_img
 
     # return non_none_elements, none_elements
 
-# Example usage
-# non_none_data, none_data = get_playerdata(api_url)
-# logging.info(f"Non-None Elements: {non_none_data}")
-# logging.info(f"None Elements: {none_data}")
+# For testing
+# get_playerdata(api_url)

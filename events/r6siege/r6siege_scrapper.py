84b38ed9bb5e31b902d0e26ca7046a046e915f18
20ArmstrongD <donald.curtis.armstrong@gmail.com>
2025-02-17 02:31:04 +0000

    updated siege scrapper and validated new version works

--- events/r6siege/r6siege_scrapper.py
+++ events/r6siege/r6siege_scrapper.py
@@ -5,121 +5,114 @@ from pyppeteer import launch
 # Logging configuration
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s',
-    datefmt='%I:%M:%S %p'  # 12-hour clock with AM/PM
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%I:%M:%S %p",  # 12-hour clock with AM/PM
 )
 
-# api_url = 'https://r6.tracker.network/r6siege/profile/ubi/BigMcD0n/overview'
 
-async def get_r6siege_player_data(api_url):
+async def get_r6siege_player_data(username, platform):
+    browser = None
+    rank = ranked_kd = rank_img = None  # Initialize variables to avoid undefined errors
+
     try:
         # Launch a headless browser
+        url = f"https://r6.tracker.network/r6siege/profile/{platform}/{username}/overview"
         browser = await launch(
             headless=True,
-            executablePath='/usr/bin/chromium-browser',  # Path to the Chromium browser installed on your system
-            args=['--no-sandbox']
+            executablePath="/usr/bin/chromium-browser",
+            args=["--no-sandbox"],
         )
         page = await browser.newPage()
 
         # Set a longer timeout if needed
-        await page.goto(api_url, {'timeout': 60000})  # Wait for the page to load for 60 seconds
+        await page.goto(url, {"timeout": 60000})
 
-        await page.waitForSelector("span", {'timeout': 60000})  # Wait for any span tag to be loaded
+        await page.waitForSelector("span", {"timeout": 60000})  # Wait for any span tag
 
-        # Use JavaScript evaluation to find the text content
-        # Use CSS selector or XPATH to find values 
-        kd = await page.evaluate('''() => {
-            const xpath = "//span[contains(text(), 'KD')]/following-sibling::span/span";
-            const result = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-            const element = result.singleNodeValue;
-            return element ? element.innerText : null;
-        }''')
-        # logging.info(f"KD found")
-        
-        level = await page.evaluate('''() => {
-            const xpath = "/html/body/div[1]/div/div[2]/div[3]/div/main/div[3]/div[2]/div[2]/div[2]/section[1]/div/div[1]/span[1]/span";
-            const result = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-            const element = result.singleNodeValue;
-            return element ? element.innerText : null;
-        }''')
-        # logging.info(f"Level found")
+        # Extract data using JavaScript evaluation
+        kd = await page.evaluate(
+            '''() => {
+                const xpath = "//span[contains(text(), 'KD')]/following-sibling::span/span";
+                const result = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+                return result.singleNodeValue ? result.singleNodeValue.innerText : null;
+            }'''
+        )
 
-        playtime = await page.evaluate('''() => {
-            const element = document.querySelector("span.text-secondary:nth-child(3) > span:nth-child(1)");
-            return element ? element.innerText : null;
-        }''')
-        # logging.info(f"Playtime found")
+        level = await page.evaluate(
+            '''() => {
+                const xpath = "/html/body/div[1]/div/div[2]/div[3]/div/main/div[3]/div[2]/div[2]/div[2]/section[1]/div/div[1]/span[1]/span";
+                const result = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+                return result.singleNodeValue ? result.singleNodeValue.innerText : null;
+            }'''
+        )
 
-        user_profile_img = await page.evaluate('''() => {
-            const element = document.querySelector(".user-avatar__image");
-            return element ? element.src : null;
-        }''')
-        # logging.info(f"Player Profile Pic found")
-
-        # Try to find ranked elements (if available)
-        try:
-            
-            rank = await page.evaluate('''() => {
-                const element = document.querySelector(".flex-1 > div:nth-child(1) > span:nth-child(1)");
+        playtime = await page.evaluate(
+            '''() => {
+                const element = document.querySelector("span.text-secondary:nth-child(3) > span:nth-child(1)");
                 return element ? element.innerText : null;
-            }''')
-            # logging.info(f"Rank found")
-            
-            ranked_kd = await page.evaluate('''() => {
-                const element = document.querySelector("div.playlist:nth-child(1) > div:nth-child(2) > div:nth-child(5) > span:nth-child(2) > span:nth-child(1)");
-                return element ? element.innerText : null;
-            }''')
-            # logging.info(f"Ranked_KD found")
-            
-            rank_img = await page.evaluate('''() => {
-                const element = document.querySelector("header.rounded-t-4 > div:nth-child(1) > img:nth-child(1)");
+            }'''
+        )
+
+        user_profile_img = await page.evaluate(
+            '''() => {
+                const element = document.querySelector(".user-avatar__image");
                 return element ? element.src : null;
-            }''')
+            }'''
+        )
 
-            # # Log the Ranked Image
-            # logging.info(f"Ranked Image found")
+        # Try to extract ranked data
+        try:
+            rank = await page.evaluate(
+                '''() => {
+                    const element = document.querySelector(".flex-1 > div:nth-child(1) > span:nth-child(1)");
+                    return element ? element.innerText : null;
+                }'''
+            )
+
+            ranked_kd = await page.evaluate(
+                '''() => {
+                    const element = document.querySelector("div.playlist:nth-child(1) > div:nth-child(2) > div:nth-child(5) > span:nth-child(2) > span:nth-child(1)");
+                    return element ? element.innerText : null;
+                }'''
+            )
+
+            rank_img = await page.evaluate(
+                '''() => {
+                    const element = document.querySelector("header.rounded-t-4 > div:nth-child(1) > img:nth-child(1)");
+                    return element ? element.src : null;
+                }'''
+            )
 
         except Exception as e:
-            ranked_elements = {"Rank": rank,
-                               "Ranked_KD": ranked_kd,
-                               "Ranked_img": rank_img}
-            logging.warning(f'These unable to be pulled')
-            for key, value in ranked_elements.items():
-                if value is None:
-                    print(f"{key}: {value}")
+            logging.warning("Unable to retrieve ranked data")
 
-        # Log the results
+        # Log extracted data
         elements = {
-            'KD': kd,
-            'Level': level,
-            'Playtime': playtime,
-            'Rank': rank,
-            'Ranked KD': ranked_kd
+            "KD": kd,
+            "Level": level,
+            "Playtime": playtime,
+            "Rank": rank,
+            "Ranked KD": ranked_kd,
         }
-        img_elements = {
-            'Player Profile Pic': user_profile_img,
-            'Ranked Image': rank_img}
+        img_elements = {"Player Profile Pic": user_profile_img, "Ranked Image": rank_img}
 
-        logging.info("Player Data Successfully Found!")
+        logging.info(f"{username} Siege Data Successfully Found!")
         for key, value in elements.items():
             if value:
                 logging.info(f"    *    {key}: {value}")
         for key, value in img_elements.items():
-            if value is not None and len(value) > 10:
-                logging.info(f"    *    {key}: url has been grabbed")
+            if value and len(value) > 10:
+                logging.info(f"    *    {key}: URL has been grabbed")
+
+        return kd, level, playtime, rank, ranked_kd, user_profile_img, rank_img
 
     except Exception as e:
         logging.error(f"Error in Pyppeteer: {e}")
-        
+        return None, None, None, None, None, None, None  # Return None values on error
+
     finally:
-            # Ensure that the browser closes
-            if browser:
-                await browser.close()  # Close the browser session
+        if browser:
+            await browser.close()  # Ensure the browser closes
 
 
-        
-    return kd, level, playtime, rank, ranked_kd, user_profile_img, rank_img
-
-
-# scrapper.py script testing
-# get_playerdata(api_url)
+# asyncio.run(get_r6siege_player_data("BigMcD0n", "ubi"))
